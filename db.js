9460c46f0ba0d0f533935c5f456894ea4b2a8bbd
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-02-12 16:17:24 +0500

    feat: add songs file

--- db.js
+++ db.js
@@ -1,25 +1,3 @@
-// mongoose.connect("mongodb://localhost:27017/fruitsDB", {
-//   useNewUrlParser: true,
-// });
-
-// mongoose.set("strictQuery", false);
-
-// const fruitSchema = new mongoose.Schema({
-//   name: String,
-//   rating: Number,
-//   review: String,
-// });
-
-// const Fruit = mongoose.model("Fruit", fruitSchema);
-
-// const fruit = new Fruit({
-//   name: "Apple",
-//   rating: 7,
-//   review: "Taste Good",
-// });
-
-// fruit.save();
-
 const mongoose = require("mongoose");
 
 module.exports = async () => {
--- models/user.js
+++ models/user.js
@@ -6,10 +6,42 @@ const passwordComplexity = require("joi-password-complexity");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  name: { type: String, required: true },
-  name: { type: String, required: true },
-  name: { type: String, required: true },
-  name: { type: String, required: true },
-  name: { type: String, required: true },
+  password: { type: String, required: true },
+  gender: { type: String, required: true },
+  month: { type: String, required: true },
+  date: { type: String, required: true },
+  year: { type: String, required: true },
+  liked: { type: [String], default: [] },
+  playlists: { type: [String], default: [] },
+  isAdmin: { type: Boolean, default: false },
 });
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      isAdmin: this.isAdmin,
+    },
+    process.env.JWTPRIVATEKEY,
+    { expiresIn: "7d" }
+  );
+  return token;
+};
+
+const validate = (user) => {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(10).required(),
+    email: Joi.string().email().required(),
+    password: passwordComplexity().required(),
+    month: Joi.string().required(),
+    date: Joi.string().required(),
+    year: Joi.string().required(),
+    gender: Joi.string().valid("male", "female", "non-binary").required(),
+  });
+  return schema.validate(user);
+};
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = { User, validate };
