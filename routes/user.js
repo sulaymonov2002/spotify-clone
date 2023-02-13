056fdd97ad98a22c141ac42eda81fec099045102
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-02-14 00:23:57 +0500

    feat: add create user codes

--- routes/user.js
+++ routes/user.js
@@ -2,4 +2,10 @@ const router = require("express").Router();
 const { User, validate } = require("../models/user");
 const bcrypt = require("bcrypt");
 
+// create user
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send({ message: error.details[0] });
+});
 
+module.exports = router;
