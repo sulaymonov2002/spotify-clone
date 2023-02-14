ce5b3c003981a4859dfc9289266541d313772c00
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-02-14 09:53:17 +0500

    feat: add new auth codes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const cors = require("cors");
 const connection = require("./db");
 const userRotes = require("./routes/users");
+const authRotes = require("./routes/auth");
+
 const app = express();
 
 connection();
@@ -11,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRotes);
+app.use("/api/login", authRotes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listen on port ${port}... `));
