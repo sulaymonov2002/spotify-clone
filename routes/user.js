231a0529042a12ceb574e4af08faacf0aa2ce1ce
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-02-14 09:34:37 +0500

    feat: add new user codes

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,27 @@ const bcrypt = require("bcrypt");
 // create user
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
-  if (error) return res.status(400).send({ message: error.details[0] });
+  if (error) return res.status(400).send({ message: error.details[0].message });
+
+  const user = await User.findOne({ email: req.body.email });
+  if (user)
+    return res
+      .status(403)
+      .send({ message: "User with given email already exist" });
+
+  const salt = await bcrypt.genSalt(Number(process.env.SALT));
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+  let newUser = await new User({
+    ...req.body,
+    password: hashPassword,
+  }).save();
+
+  newUser.password = undefined;
+  newUser.__v = undefined;
+
+  res
+    .status(200)
+    .send({ data: newUser, message: "Account created successfully" });
 });
 
 module.exports = router;
