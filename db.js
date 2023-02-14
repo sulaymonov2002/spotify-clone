50dc3139eaa5c2450d13f11e87675a373ecdaa60
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-02-14 14:49:18 +0500

    feat:  add new codes for user file

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+mongoose.set("strictQuery", true);
+
 module.exports = async () => {
   const connectionParams = {
     useNewUrlParser: true,
--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ router.post("/", async (req, res) => {
     return res.status(400).send({ message: "Invalid email or password" });
 
   const token = user.generateAuthToken();
-  res.status(200).send({ message: "Signing in please wait..." });
+  res.status(200).send({ data: token, message: "Signing in please wait..." });
 });
 
 module.exports = router;
