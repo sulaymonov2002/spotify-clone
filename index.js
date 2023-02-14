4a7b4046d1bde2d53478b1f8280b484f813cbd63
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-02-14 09:44:20 +0500

    feat: done all codes users.js and create auth.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
+const cors = require("cors");
 const connection = require("./db");
+const userRotes = require("./routes/users");
 const app = express();
 
 connection();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRotes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listen on port ${port}... `));
