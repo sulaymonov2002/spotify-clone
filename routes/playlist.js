e58fa674c4b2464bf25b4b2cb35dfc69372afb8f
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-02-18 23:27:41 +0500

    feat: add new playlist.js file codes

--- routes/playlist.js
+++ routes/playlist.js
@@ -5,6 +5,7 @@ const { User } = require("../models/user");
 const auth = require("../middleware/auth");
 const validObjectId = require("../middleware/validObjectId");
 const Joi = require("joi");
+const { send } = require("process");
 
 // create playlist
 router.post("/", auth, async (req, res) => {
@@ -12,6 +13,11 @@ router.post("/", auth, async (req, res) => {
   if (error) return res.status(400).send({ message: error.details[0].message });
 
   const user = await User.findById(req.user._id);
+  const playlist = await Playlist({ ...req.body, user: user._id }).save();
+  user.playlists.push(playlist._id);
+  await user.save();
+
+  res.status(201).send({ data: playlist });
 });
 
 module.exports = router;
